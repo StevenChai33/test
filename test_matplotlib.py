84fe8796b9d1e3890f20ab676aff7e00626f6c8b
StevenChai33 <yinshengchai@163.com>
2021-03-24 18:55:36 +0800

    测试matplotlib

---
+++ test_matplotlib.py
@@ -0,0 +1,27 @@
+from sklearn import tree
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import graphviz
+from matplotlib import pyplot as plt
+import time
+
+
+# 导入数据，分训练测试集
+wine = load_wine()
+X_train, X_test, y_train, y_test = train_test_split(wine['data'], wine['target'], test_size=0.3)
+
+# 最大深度学习曲线
+score_list = []
+
+for i in range(1, 11):
+    clf = tree.DecisionTreeClassifier(max_depth=i)
+    clf = clf.fit(X_train, y_train)
+    score = clf.score(X_test, y_test)
+    score_list.append(score)
+
+print(score_list)
+plt.plot(range(1, 11), score_list, label='max_depth', color='blue')
+plt.legend()
+plt.show()
+time.sleep(1000)
